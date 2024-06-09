256bbe7e72bfe009adcd6a60e7b800036fd55993
dennisppaul <dennis.paul@hfk-bremen.de>
2024-06-09 18:57:48 +0200

    minor changes

--- Klangstrom/examples/TestCleanCaterpillar/TestCleanCaterpillar.ino
+++ Klangstrom/examples/TestCleanCaterpillar/TestCleanCaterpillar.ino
@@ -1,8 +1,15 @@
 #include "Arduino.h"
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello!");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    delay(1000);
+  Serial.println("3");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
