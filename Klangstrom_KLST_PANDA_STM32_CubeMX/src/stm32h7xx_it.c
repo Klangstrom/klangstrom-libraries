18265b14278448836b79e1f3ad85efa1a39fa186
dennisppaul <dennis.paul@hfk-bremen.de>
2024-08-16 08:16:34 +0200

    removed USB Device

--- Klangstrom_KLST_PANDA_STM32_CubeMX/src/stm32h7xx_it.c
+++ Klangstrom_KLST_PANDA_STM32_CubeMX/src/stm32h7xx_it.c
@@ -57,7 +57,6 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern PCD_HandleTypeDef   hpcd_USB_OTG_HS;
 extern DMA_HandleTypeDef   hdma_dfsdm1_flt0;
 extern DMA_HandleTypeDef   hdma_dfsdm1_flt1;
 extern DMA2D_HandleTypeDef hdma2d;
@@ -442,45 +441,6 @@ void DMA2_Stream1_IRQHandler(void) {
     /* USER CODE BEGIN DMA2_Stream1_IRQn 1 */
     // #endif // !defined(ARDUINO_KLST_PANDA)
     /* USER CODE END DMA2_Stream1_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USB On The Go HS End Point 1 Out global interrupt.
-  */
-void OTG_HS_EP1_OUT_IRQHandler(void) {
-    /* USER CODE BEGIN OTG_HS_EP1_OUT_IRQn 0 */
-
-    /* USER CODE END OTG_HS_EP1_OUT_IRQn 0 */
-    HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
-    /* USER CODE BEGIN OTG_HS_EP1_OUT_IRQn 1 */
-
-    /* USER CODE END OTG_HS_EP1_OUT_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USB On The Go HS End Point 1 In global interrupt.
-  */
-void OTG_HS_EP1_IN_IRQHandler(void) {
-    /* USER CODE BEGIN OTG_HS_EP1_IN_IRQn 0 */
-
-    /* USER CODE END OTG_HS_EP1_IN_IRQn 0 */
-    HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
-    /* USER CODE BEGIN OTG_HS_EP1_IN_IRQn 1 */
-
-    /* USER CODE END OTG_HS_EP1_IN_IRQn 1 */
-}
-
-/**
-  * @brief This function handles USB On The Go HS global interrupt.
-  */
-void OTG_HS_IRQHandler(void) {
-    /* USER CODE BEGIN OTG_HS_IRQn 0 */
-
-    /* USER CODE END OTG_HS_IRQn 0 */
-    HAL_PCD_IRQHandler(&hpcd_USB_OTG_HS);
-    /* USER CODE BEGIN OTG_HS_IRQn 1 */
-
-    /* USER CODE END OTG_HS_IRQn 1 */
 }
 
 /**
