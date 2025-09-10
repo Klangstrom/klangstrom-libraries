0813519dccad72c0fd4972061ae7e74361477c15
dennisppaul <dennis.paul@hfk-bremen.de>
2025-09-10 13:14:08 +0200

    Squashed 'libraries/' changes from d036338..d0cd72a
    d0cd72a changed backlight PWM frequency
    
    git-subtree-dir: libraries
    git-subtree-split: d0cd72aea27642fa9eaa1a06ed8d2ddcecb2fa56

--- Klangstrom_KLST_PANDA_STM32_CubeMX/src/tim.c
+++ Klangstrom_KLST_PANDA_STM32_CubeMX/src/tim.c
@@ -155,9 +155,9 @@ void MX_TIM3_Init(void) {
 
     /* USER CODE END TIM3_Init 1 */
     htim3.Instance               = TIM3;
-    htim3.Init.Prescaler         = 0;
+    htim3.Init.Prescaler         = 17;
     htim3.Init.CounterMode       = TIM_COUNTERMODE_UP;
-    htim3.Init.Period            = 32768;
+    htim3.Init.Period            = 255;
     htim3.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
     htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
     if (HAL_TIM_PWM_Init(&htim3) != HAL_OK) {
@@ -169,7 +169,7 @@ void MX_TIM3_Init(void) {
         Error_Handler();
     }
     sConfigOC.OCMode     = TIM_OCMODE_PWM1;
-    sConfigOC.Pulse      = 16383;
+    sConfigOC.Pulse      = 127;
     sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
     sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
     if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK) {
