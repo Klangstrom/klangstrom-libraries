ebd85793722077350f1811943395cd109872bb2d
dennisppaul <dennis.paul@hfk-bremen.de>
2025-09-10 08:44:26 +0200

    cleaned up scripts (WIP)

--- tools/klangstrom-install-libraries.sh
+++ tools/klangstrom-install-libraries.sh
@@ -3,4 +3,4 @@
 # TODO WIP this os macOS only for now
 # TODO lib path is adapted from `../libraries/klangstrom-libraries` to `../libraries`
 
-./link_libraries.sh klangstrom-libraries.txt ../libraries "$HOME/Documents/Arduino/libraries"
+./link_libraries.sh klangstrom-libraries.config ../libraries "$HOME/Documents/Arduino/libraries"
